857a3ac7d00da2cb7704ca85bf1626933087b0d7
vitaliy <fvetalika2307@gmail.com>
2020-10-07 15:21:32 +0300

    First t

--- src/main/java/ua/com/alevel/jdbc/SQLConnection.java
+++ src/main/java/ua/com/alevel/jdbc/SQLConnection.java
@@ -30,7 +30,7 @@ public class SQLConnection {
         ResultSet set = null;
         try {
             statement = connection.createStatement();
-            set = statement.executeQuery("SELECT * FROM offices WHERE territory = 'EMEA';");
+            set = statement.executeQuery("SELECT * FROM offices;");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
